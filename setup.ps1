b73e52f544ef1ad58c0f84f4fdcdf2b13fb59c20
Divya Rasania <70239770+DivyaRasania@users.noreply.github.com>
2024-08-22 11:45:30 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -9,7 +9,7 @@ if (!$principal.IsInRole($adminRole)) {
     break
 }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 
 $answer = Read-Host "Do you want to install JavaJDK? [y/N]"
